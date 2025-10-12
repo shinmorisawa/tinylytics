2cb9603c5a0568bb375cab7cbc494cd8631b4774
Shin Morisawa <tryh4rd3rr@proton.me>
2025-10-12 21:18:18 +0900

    wait why was the js like that

--- js/track.js
+++ js/track.js
@@ -1,17 +1,24 @@
-const ip = await fetch("api.tryh4rd.dev/tiny/ip", {
-    method: "GET"
-});
+async function main() {
+    const ip = await fetch("https://api.tryh4rd.dev/tiny/ip", {
+        method: "GET"
+    });
 
-const encoder = new TextEncoder();
-const data = encoder.encode(ip + navigator.userAgent);
-const hashBuffer = await crypto.subtle.digets("SHA-512", data);
-const hashArray = Array.from(new Uint8Array(hashBuffer));
-const hashHex = hashArray.map(b => b.toString(16).padStart(2,'0')).join('');
-const path = window.location.pathname;
-const finalString = hashHex + path;
+    const encoder = new TextEncoder();
+    const data = encoder.encode(ip + navigator.userAgent);
+    const hashBuffer = await crypto.subtle.digest("SHA-512", data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2,'0')).join('');
+    var path = window.location.pathname;
+    path = path.normalize('NFC');
+    const finalString = hashHex + path;
 
-await fetch("api.tryh4rd.dev/tiny/track", {
-    method: "POST",
-    headers: { "Content-Type": "text/plain" },
-    body: finalString
-});
+    console.log(path);
+
+    await fetch("https://api.tryh4rd.dev/tiny/track", {
+        method: "POST",
+        headers: { "Content-Type": "text/plain" },
+        body: finalString
+    });
+}
+
+main();
